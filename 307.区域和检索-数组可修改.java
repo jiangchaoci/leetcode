74a2a84eaf5fe0c38f468fc00b4d7e65167b531d
jiangchaoci <jiangchaoci@163.com>
2020-12-04 17:40:10 +0800

    20201204

--- 307.区域和检索-数组可修改.java
+++ 307.区域和检索-数组可修改.java
@@ -6,17 +6,25 @@
 
 // @lc code=start
 class NumArray {
+    private int[] dp;
+    private int[] tNums;
 
     public NumArray(int[] nums) {
-
+        dp = new int[nums.length + 1];
+        tNums = new int[nums.length];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = dp[i-1] + nums[i-1];
+            tNums[i-1] = nums[i-1];
+        }
     }
     
     public void update(int i, int val) {
-
+        int dif = val - tNums[i];
+        for (int j = i; j < )
     }
     
     public int sumRange(int i, int j) {
-
+        return dp[j+1] - dp[i];
     }
 }
 
